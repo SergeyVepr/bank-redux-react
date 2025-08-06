d011c63d0d5d89d14cb63bb7c46b7302fdefc245
«SergeyVepr» <lolssan@icloud.com>
2025-08-06 18:30:03 +0300

    add redux from Redux!

--- src/configureStore/store.js
+++ src/configureStore/store.js
@@ -1,8 +1,9 @@
-import {createStore} from "../redux/createStore.js";
+
 import {accountReducer} from "../reducers/accountReducer.js";
+import {legacy_createStore} from "redux";
 
 const initialState = {
     balance: 0,
 }
 
-export const store = createStore(accountReducer, initialState);
+export const store = legacy_createStore(accountReducer, initialState);
